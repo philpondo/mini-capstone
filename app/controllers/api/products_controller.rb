f2226d35f05f0e55abf8e9675867354da1ded337
phil <phil.pondo@gmail.com>
2020-05-27 13:50:00 -0400

    adds single product route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,10 @@ def all_products
   @all_products = Product.all
   render 'all_products.json.jb'
 end
+
+def single_product
+  @single_product = Product.all.sample
+  render 'single_product.json.jb'
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#all_products"
+    get "/single_product" => "products#single_product"
   end
 end
